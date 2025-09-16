ef9b1a170a75c3409e74eec38557285d487d1c93
leotechru <leotechru@ya.ru>
2025-09-17 01:19:57 +0300

    Commit message

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,14 +40,14 @@ def _build_async_url(schema: str) -> str:
 def _build_sync_url() -> str:
     base_url = os.getenv('DATABASE_URL')
     if base_url:
-        sync_url = base_url.replace('+asyncpg', '+psycopg')
+        sync_url = base_url.replace('+asyncpg', '')
     else:
         user = os.getenv('DB_USER', '')
         password = os.getenv('DB_PASSWORD', '')
         host = os.getenv('DB_HOST', 'localhost')
         port = os.getenv('DB_PORT', '5432')
         name = os.getenv('DB_NAME', '')
-        sync_url = f'postgresql+psycopg://{user}:{password}@{host}:{port}/{name}'
+        sync_url = f'postgresql://{user}:{password}@{host}:{port}/{name}'
     parts = urlsplit(sync_url)
     return urlunsplit((parts.scheme, parts.netloc, parts.path, '', parts.fragment))
 
